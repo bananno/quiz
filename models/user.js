38472f993bf24ac1e0d61582f8564ba3d2066042
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-05 14:00:53 -0800

    Create user model

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.statics.authenticate = (username, password, callback) => {
+  User.findOne({ username: username }, findUser);
+};
+
+function findUser(error, user) {
+  if (error) {
+    return callback(error);
+  }
+
+  if (!user) {
+    error = new Error('User not found.');
+    error.status = 401;
+    return callback(error);
+  }
+
+  bcrypt.compare(password, user.password, (error, result) => {
+    if (result === true) {
+      return callback(null, user);
+    }
+    return callback();
+  });
+}
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
