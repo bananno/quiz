07d45c162f351584b38b4f2beb9afe80c79d2f55
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-05 14:11:50 -0800

    Add tests

--- test/models.user.test.js
+++ test/models.user.test.js
@@ -2,10 +2,10 @@ const expect = require('chai').expect;
 const User = require('../models/user');
 
 describe('User', function() {
-  it('validation returns no error for valid object', function (done) {
+  it('validation returns no error for valid object', done => {
     var user = new User({
-      username: "samplename",
-      password: "password123"
+      username: 'samplename',
+      password: 'password123'
     });
     user.validate(err => {
       expect(err).to.be.null;
@@ -13,10 +13,30 @@ describe('User', function() {
     });
   });
 
-  it('should be invalid without username', function (done) {
+  it('should be invalid without any attributes', done => {
     var user = new User();
     user.validate(err => {
       expect(err.errors.username).to.exist;
+      done();
+    });
+  });
+
+  it('should be invalid without username', done => {
+    var user = new User({
+      password: 'password123'
+    });
+    user.validate(err => {
+      expect(err.errors.username).to.exist;
+      done();
+    });
+  });
+
+  it('should be invalid without password', done => {
+    var user = new User({
+      username: 'samplename'
+    });
+    user.validate(err => {
+      expect(err.errors.password).to.exist;
       done();
     });
   });
