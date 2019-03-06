05843aab9f5821a65669ca10489ca3c3cc715455
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-05 20:38:53 -0800

    Begin adding login and signup routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.get('/', getHomePage);
+router.post('/login', loginUser);
+router.post('/signup', signupUser);
 
 function getHomePage(req, res, next) {
   res.render('index');
+}
+
+function loginUser(req, res, next) {
+  res.redirect('/');
+}
+
+function signupUser(req, res, next) {
+  res.redirect('/');
 }
 
 module.exports = router;
