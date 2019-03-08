2a890bc60c640243c1d2c60c47c58cbce265a05c
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-08 15:59:57 -0800

    Add tests for index UI

--- test-casper/index.test.js
+++ test-casper/index.test.js
@@ -1,7 +1,11 @@
+before(function() {
+  casper.start('http://localhost:4300');
+});
+
+after(function() {
+});
+
 describe('Index view', function() {
-  before(function() {
-    casper.start('http://localhost:4300');
-  });
 
   it('should have a login form', function(){
     casper.waitForSelector('#login-form', function() {
@@ -12,6 +16,30 @@ describe('Index view', function() {
   it('should have a signup form', function(){
     casper.waitForSelector('#signup-form', function() {
       '#signup-form'.should.be.inDOM;
+    });
+  });
+});
+
+describe('Signup form', function() {
+  it('should have username and password fields', function() {
+    casper.then(function() {
+      this.fillSelectors('#signup-form form', {
+        '[name="username"]' : 'new_username',
+        '[name="password"]' : '123'
+      });
+    });
+  });
+
+  it('should redirect', function() {
+    casper.thenClick('#signup-form button');
+
+    casper.waitFor(function check() {
+      return this.evaluate(function() {
+        return /signup/.test(document.location.pathname);
+      });
+    }, function then() {
+    }, function timeout() {
+      this.echo('Failed to load page').exit();
     });
   });
 });
