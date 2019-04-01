8f350c120491154a3a8bca415a5ec4a56f26bb4c
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-01 11:43:23 -0700

    Show number of cards created by current user

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const getUser = require('./getUser');
+const Card = require('../models/card');
 
 router.get('/profile', (req, res, next) => {
   getUser(req, res, next, user => {
-    res.render('layout', {
-      view: 'profile',
-      title: 'Profile',
+    Card.find({ owner: user }, (error, cards) => {
+      if (error) {
+        return next(error);
+      }
+
+      res.render('layout', {
+        view: 'profile',
+        title: 'Profile',
+        cards: cards,
+      });
     });
   });
 });
