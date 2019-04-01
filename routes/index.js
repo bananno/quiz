49bf8f97e500cff181a4c457ab8c3e3d1dd85d09
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-01 11:41:05 -0700

    Remove extra route from index file

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const getUser = require('./getUser');
-
-router.get('/profile', getProfilePage);
 
 router.get('/', (req, res, next) => {
   res.redirect('profile');
 });
-
-function getProfilePage(req, res, next) {
-  getUser(req, res, next, user => {
-    res.render('layout', {
-      view: 'profile',
-      title: 'Profile',
-    });
-  });
-}
 
 module.exports = router;
