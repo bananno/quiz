cc8099be3b03c8dbb9a4d8f704d5f1c491133e0e
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-01 11:23:31 -0700

    Add form and route for creating new card

--- routes/cards.js
+++ routes/cards.js
@@ -12,4 +12,28 @@ router.get('/newCard', (req, res, next) => {
   });
 });
 
+router.post('/newCard', (req, res, next) => {
+  getUser(req, res, next, user => {
+    const cardData = {
+      question: req.body.question,
+      answer: req.body.answer,
+      owner: user,
+    };
+
+    if (!cardData.question || !cardData.answer) {
+      const error = new Error('All fields are required.');
+      error.status = 412;
+      return next(error);
+    }
+
+    Card.create(cardData, (error, card) => {
+      if (error) {
+        return next(error);
+      }
+
+      return res.redirect('/profile');
+    });
+  });
+});
+
 module.exports = router;
