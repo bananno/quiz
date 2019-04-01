41ad50e4ba99c31e4f617aaed92bdaf769974edc
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-01 11:18:49 -0700

    Add view for creating new card

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
 const Card = require('../models/card');
+const getUser = require('./getUser');
+
+router.get('/newCard', (req, res, next) => {
+  getUser(req, res, next, user => {
+    res.render('layout', {
+      view: 'newCard',
+      title: 'New Card',
+    });
+  });
+});
 
 module.exports = router;
