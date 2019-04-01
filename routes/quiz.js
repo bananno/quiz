cb214613a3e8e24e7a78c327632c43943fdaebe2
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-01 12:54:04 -0700

    Use link to iterate through cards

--- routes/quiz.js
+++ routes/quiz.js
@@ -4,20 +4,29 @@ const Card = require('../models/card');
 const getUser = require('./getUser');
 
 router.get('/quiz', (req, res, next) => {
+  res.redirect('/quiz/0');
+});
+
+router.get('/quiz/:index', (req, res, next) => {
+  let index = parseInt(req.params.index || 0);
   getUser(req, res, next, user => {
     Card.find({ owner: user }, (error, cards) => {
+      if (error) {
+        return next(error);
+      }
+
+      if (!cards[index]) {
+        return res.redirect('/profile');
+      }
+
       res.render('layout', {
         view: 'quiz',
         cards: cards,
-        card: cards[0],
-        index: 0,
+        card: cards[index],
+        index: index,
       });
     });
   });
-});
-
-router.get('/next', (req, res, next) => {
-  res.redirect('/quiz');
 });
 
 module.exports = router;
