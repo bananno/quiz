70d1380fd0c15992910f14afb092248b2cee763f
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-01 16:11:17 -0700

    Show card lists on profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,15 +2,18 @@ const express = require('express');
 const router = express.Router();
 const getUser = require('./getUser');
 const getCards = require('./getCards');
+const CardList = require('../models/cardList');
 
 router.get('/profile', (req, res, next) => {
   getUser(req, res, next, user => {
     getCards(req, res, next, { owner: user }, cards => {
-      res.render('layout', {
-        view: 'profile',
-        title: 'Profile',
-        cards: cards,
-        cardLists: [{ title: 'My Card List' }],
+      CardList.find({ owner: user }, (error, cardLists) => {
+        res.render('layout', {
+          view: 'profile',
+          title: 'Profile',
+          cards: cards,
+          cardLists: cardLists,
+        });
       });
     });
   });
